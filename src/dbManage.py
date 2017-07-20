99b08802ac49d7ee7d444cd6df22b976066ee06b
amjltc295 <b03901014@ntu.edu.tw>
2017-07-20 15:19:53 +0800

    Change migration location

--- src/dbManage.py
+++ src/dbManage.py
@@ -1,13 +1,14 @@
 import os
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 from API import app, db
 
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-migrate = Migrate(app, db)
+
+migrate = Migrate(app, db, directory='src/migrations')
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
