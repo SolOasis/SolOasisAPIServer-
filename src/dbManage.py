1a176f8f45c8a5dc693a9fea78196cd52f1ea6cf
amjltc295 <b03901014@ntu.edu.tw>
2017-07-25 10:33:29 +0800

    db error fixing

--- src/dbManage.py
+++ src/dbManage.py
@@ -11,12 +11,14 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 migrate = Migrate(app, db, directory='src/migrations')
 manager = Manager(app)
 
-manager.add_command('db', MigrateCommand)
-
 @manager.command
 def clearAlembic():
     from dbmodel import Alembic
     Alembic.clear_A()
+
+manager.add_command('db', MigrateCommand)
+manager.add_command('db', clearAlembic)
+
 
 if __name__ == '__main__':
     manager.run()
